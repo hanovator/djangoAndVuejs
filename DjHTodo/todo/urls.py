f0df3f8b5882970c08a6881913ad4df1cfec6236
HAN <hanovator@gmail.com>
2019-08-09 17:50:13 +0900

    Mixin을 이용한 todo

--- DjHTodo/todo/urls.py
+++ DjHTodo/todo/urls.py
@@ -9,4 +9,7 @@ urlpatterns = [
     path('create/', views.TodoCV.as_view(), name='create'),
     path('lsit/', views.TodoLV.as_view(), name='list'),
     path('<int:pk>/delete/', views.TodoDelV.as_view(), name='delete'),
+
+    path('mixin/', views.TodoMOMCV.as_view(), name='mixin'),
+    path('<int:pk>/delete2/', views.TodoDelV2.as_view(), name='delete2'),
 ]
--- DjHTodo/todo/views.py
+++ DjHTodo/todo/views.py
@@ -1,5 +1,6 @@
 from django.urls import reverse_lazy
 from django.views.generic import TemplateView, CreateView, ListView, DeleteView
+from django.views.generic.list import MultipleObjectMixin
 
 from todo.models import Todo
 
@@ -25,3 +26,23 @@ class TodoDelV(DeleteView):
     template_name = 'todo/todo_confirm_delete.html'
     success_url = reverse_lazy('todo:list')
 
+class TodoMOMCV(MultipleObjectMixin, CreateView): # (mixin, main)순서로 적어준다.
+    model = Todo
+    fields = '__all__'
+    template_name = 'todo/todo_form_list.html'
+    success_url = reverse_lazy('todo:mixin')
+
+    def get(self, request, *args, **kwargs):
+        self.object_list = self.get_queryset()
+        return super().get(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        self.object_list = self.get_queryset()
+        return super().post(request, *args, **kwargs)
+
+class TodoDelV2(DeleteView):
+    model = Todo
+    success_url = reverse_lazy('todo:mixin')
+
+    def get(self, request, *args, **kwargs):
+        return self.delete(request, *args, **kwargs)
